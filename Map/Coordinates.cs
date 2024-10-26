1922b401bdfa3743512d9b2aac96bc1011a3450b
Egor Gavrilenko <bambloo769@gmail.com>
2024-10-26 19:03:01 +0300

    feat: implement Coordinates type

---
+++ Map/Coordinates.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Simulation.Map
+{
+    internal record Coordinates
+    {
+        public int Row { get; }
+        public int Col { get; }
+
+        public Coordinates(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public override string ToString()
+        {
+            return $"Simulation.Map.Coordinates {{ Row: {Row}; Col: {Col} }}";
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Col);
+        }
+
+
+    }
+}
