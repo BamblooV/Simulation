64481c02e74b4c677de71bc2bf39be7777087c89
Egor Gavrilenko <bambloo769@gmail.com>
2024-10-26 19:10:05 +0300

    feat: abstract Entity type

---
+++ Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿using Simulation.Map;
+
+namespace Simulation.Entities
+{
+    internal abstract class Entity
+    {
+        public Coordinates Coordinates { get; set; }
+
+        public Entity(Coordinates coordinates) => Coordinates = coordinates;
+    }
+}
